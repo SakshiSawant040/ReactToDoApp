eb5f044f3c9cec88a7e2f786fe943b6585a80bfa
Sakshi <sakshirsawant040@gmail.com>
2023-07-13 22:48:50 +0530

    modified

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -1,25 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect,useRef } from 'react'
 import './Addtask.css'
-
 
 
 export default function AddTask() {
     const [task,getTask] = useState('');
-    const [listTasks,setTask]=useState([]);
+    const [listTasks,setTask] = useState([]);
+    const ref = useRef(null);
 
-    const handleinput=(e)=>{
-        getTask(e.target.value)
-    }
     const addTask=() =>{
         console.log('clicked');
         setTask((inputTask) =>{
             return [...inputTask,task];
-        })
+    })
+
+    // useEffect(() =>{
+    //     const listtask=ref.current;
+    //       listtask.addEventListener("click",function(e){
+    //         if(e.target.tagName === "LI"){
+    //           e.target.classList.toggle("checked")
+    //         }
+    //         else if(e.target.tagName === "SPAN"){
+    //           e.target.parentElement.remove();
+    //         }
+    //       },false)
+    // })
+        
+        
+
     }
   return (
     <div>
       <div className="inputTask">
-        <input type="text"  placeholder='Add your task' id='input-box' onChange={handleinput}/>
+        <input type="text"  placeholder='Add your task' id='input-box' onChange={(e)=>getTask(e.target.value)}/>
         <button onClick={addTask}>Add</button>
       </div>
       
@@ -28,8 +40,11 @@ export default function AddTask() {
         {/* <li>{task}</li> */}
 
         {
-              listTasks.map((taskN) =>{
-                return <li>{taskN}</li>
+              listTasks.map((element,i) =>{
+                return <li className='checked' ref={ref}>
+                          {element}
+                          <span>{'\u00d7'}</span>
+                        </li>
               })
         }
       </ul>
